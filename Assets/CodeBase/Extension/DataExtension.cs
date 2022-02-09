a8d501bf346e048b16681f55129fc5e3317b6c59
Efim Socolov <aafanya@bk.ru>
2022-02-10 01:10:49 +0700

    Adding AI

--- Assets/CodeBase/Extension/DataExtension.cs
+++ Assets/CodeBase/Extension/DataExtension.cs
@@ -4,6 +4,11 @@ namespace CodeBase.Extension
 {
     public static class DataExtension
     {
+        public static float CosAngle(this Vector3 center, Vector3 vectorA, Vector3 vectorB) => 
+            ((vectorB.x - vectorA.x) * (center.z - vectorA.z) - (center.x - vectorA.x) * (vectorB.z - vectorA.z)) /
+            (Mathf.Sqrt(Mathf.Pow(vectorB.x - vectorA.x, 2) + Mathf.Pow(vectorB.z - vectorA.z, 2)) *
+             Mathf.Sqrt(Mathf.Pow(center.x - vectorA.x, 2) + Mathf.Pow(center.z - vectorA.z, 2)));
+
         public static string SerializeToJson(this object obj) => 
             JsonUtility.ToJson(obj);
 
--- Assets/CodeBase/Services/Factories/Enemy/EnemyMovement.cs
+++ Assets/CodeBase/Services/Factories/Enemy/EnemyMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using CodeBase.Extension;
 using CodeBase.Logic.Car;
 using UnityEngine;
 using UnityEngine.AI;
@@ -14,11 +16,21 @@ namespace CodeBase.Services.Factories.Enemy
 
         private Transform _target;
         
-        public void Construct(Transform target)
-        {
+        public void Construct(Transform target) => 
             _target = target;
+
+        private void Update()
+        {
+            _navMeshAgent.transform.position = transform.position;
+            _navMeshAgent.destination = _target.position;
+
+            double angle = Math.Round(CosAngle() / 0.01f, 4);
             
-            _navMeshAgent.SetDestination(target.position);
+            _car.Rotation(Mathf.Clamp((float)angle, -1, 1));
+            _car.Movement(Mathf.Clamp(_navMeshAgent.path.corners[1].magnitude, -1, 1));
         }
+
+        private float CosAngle() => 
+            transform.position.CosAngle(_navMeshAgent.path.corners[1], transform.position + transform.forward);
     }
 }
